141a8d11b37906dddd58f25d6d2d76b1a10c4607
Patrick Towles <james.towles@gmail.com>
2020-05-12 02:57:15 -0400

    cleaned up api/files function

--- backend/src/routes/api.rs
+++ backend/src/routes/api.rs
@@ -10,22 +10,17 @@ struct File {
     size: u64,
 }
 
+// Returns list of files in current directory
 #[get("/api/files")]
 pub fn files() -> JsonValue {
     let mut results: Vec<File> = vec![];
     
-    //use std::env;
-    // let path = env::current_dir().unwrap();
-    
     if let Ok(entries) = fs::read_dir(".") { 
         for entry in entries {        
             if let Ok(entry) = entry {
-                
-                //files.push(File { name: entry.file_name() });
-                println!("{:?}", entry.file_name());
                 let metadata = entry.metadata().unwrap();
-
                 if metadata.is_file() {
+                    println!("file: {}", entry.path().display());
                     results.push(File { 
                         name: entry.file_name().into_string().unwrap(), 
                         size: metadata.len(),
